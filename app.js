db96b8282a54a0ade284ae97ad240fcdbfb7c5d3
mustafa-mahmud <mithuweb000@gmail.com>
2021-12-04 21:43:17 +0600

    end project

--- app.js
+++ app.js
@@ -3,11 +3,14 @@
 const textarea = document.querySelector('.keyboard-input');
 const keyboard = document.querySelector('.keyboard');
 const keyboardKeys = document.querySelectorAll('.keyboard__key');
+const backspace = document.querySelector('.backspace');
+const capslock = document.querySelector('.capslock');
 
 keyboardKeys.forEach((el) => {
   const key = el.textContent.toLowerCase().trim();
   if (key === 'space_bar') el.setAttribute('data-key', '');
-  else if (key === 'keyboard_return') el.setAttribute('data-key', 'Enter');
+  else if (key === 'keyboard_return') el.setAttribute('data-key', 'enter');
+  else if (key === 'keyboard_capslock') el.setAttribute('data-key', 'capslock');
   else el.setAttribute('data-key', key);
 });
 
@@ -24,15 +27,19 @@ function showHideKeyboard(e) {
   }
 }
 
-function typeTextarea(e) {
-  //fix:: backspace not get
-  const x = e.which || e.keyCode;
-  console.log(x);
-  console.log(e.key.trim());
+function hideKeyboard() {
+  keyboard.classList.add('keyboard--hidden');
+}
 
-  return;
-  const typed = e.key.trim();
-  console.log(typed);
+function typeTextarea(e) {
+  const isCapsOnOff = e.getModifierState('CapsLock');
+
+  if (isCapsOnOff) toggleCapslock(true);
+  else toggleCapslock(false);
+
+  keyboard.classList.remove('keyboard--hidden');
+  const typed = e.key.trim().toLowerCase();
+
   keyboardKeys.forEach((key) => {
     if (key.getAttribute('data-key') === typed) {
       key.classList.add('active');
@@ -41,6 +48,77 @@ function typeTextarea(e) {
   });
 }
 
+function toggleCapslock(bool) {
+  if (bool) capslock.classList.add('keyboard__key--active');
+  else capslock.classList.remove('keyboard__key--active');
+
+  convertText();
+}
+
+function convertText() {
+  const isActive = capslock.classList.contains('keyboard__key--active');
+  if (isActive) {
+    for (let i = 0; i < keyboardKeys.length; i++) {
+      if (keyboardKeys[i].textContent === 'keyboard_capslock') continue;
+      else if (keyboardKeys[i].textContent === 'backspace') continue;
+      else if (keyboardKeys[i].textContent === 'keyboard_return') continue;
+      else if (keyboardKeys[i].textContent === 'check_circle') continue;
+      else if (keyboardKeys[i].textContent === 'space_bar') continue;
+      else
+        keyboardKeys[i].textContent = keyboardKeys[i].textContent.toUpperCase();
+    }
+  } else {
+    for (let i = 0; i < keyboardKeys.length; i++) {
+      if (keyboardKeys[i].textContent === 'keyboard_capslock') continue;
+      else if (keyboardKeys[i].textContent === 'backspace') continue;
+      else if (keyboardKeys[i].textContent === 'keyboard_return') continue;
+      else if (keyboardKeys[i].textContent === 'check_circle') continue;
+      else if (keyboardKeys[i].textContent === 'space_bar') continue;
+      else
+        keyboardKeys[i].textContent = keyboardKeys[i].textContent.toLowerCase();
+    }
+  }
+}
+
+function clickKeys(e) {
+  const isKeyboardKeys = e.target.closest('.keyboard__key');
+  if (!isKeyboardKeys) return;
+  textarea.focus();
+  e.target.classList.add('active');
+  setTimeout(() => e.target.classList.remove('active'), 200);
+
+  const ckClick = e.target.textContent;
+  if (ckClick === 'keyboard_capslock') {
+    const ckCapslock = capslock.classList.contains('keyboard__key--active');
+    if (ckCapslock) toggleCapslock(false);
+    else toggleCapslock(true);
+    return;
+  }
+  if (ckClick === 'space_bar') {
+    textarea.value += ' ';
+    return;
+  }
+  if (ckClick === 'check_circle') {
+    hideKeyboard();
+    return;
+  }
+  if (ckClick === 'keyboard_return') {
+    textarea.value += '\n';
+    return;
+  }
+  if (ckClick === 'backspace') {
+    let arr = [...textarea.value];
+    arr.pop();
+    textarea.value = arr.join('');
+    return;
+  }
+
+  const whereClick = e.target.textContent;
+  textarea.value += whereClick;
+}
+
 /////////////////////////
+convertText();
 document.addEventListener('click', showHideKeyboard);
-textarea.addEventListener('keypress', typeTextarea);
+textarea.addEventListener('keyup', typeTextarea);
+keyboard.addEventListener('click', clickKeys);
